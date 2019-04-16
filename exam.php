87a9e1e37aba6503ee82676bd9b6dcdf29811992
cuimeixiao <49506924+cuimeixiao@users.noreply.github.com>
2019-04-16 08:56:27 +0800

    Add files via upload

---
+++ exam.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1
+ * Date: 2019/4/16
+ * Time: 8:48
+ */
+$ji = '';
+$str = array(1,2,3,4,5,6,7);
+for ($i = 1;$i<=count($str);$i++){
+      if($str/2)
+      {
+          print_r($str);
+      }
+      else
+      {
+          print_r($str);
+      }
+      print_r($ji.$str);
+}
