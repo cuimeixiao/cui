e34a3c053c4d36205e3c3254997f9fb3a000fcec
cuimeixiao <49506924+cuimeixiao@users.noreply.github.com>
2019-04-18 09:01:03 +0800

    4.18

--- one.php
+++ one.php
@@ -2,25 +2,29 @@
 /**
  * Created by PhpStorm.
  * User: 1
- * Date: 2019/4/17
- * Time: 8:39
+ * Date: 2019/4/18
+ * Time: 8:41
  */
-
-function calFn($a,$b){
-    //定义和为0；
-    $sum = 0;
-//for循环判断
-    for ($i=$a;$i<=$b;$i++){
-        //判断个位为1的个数
-        if($i%10 == 1){
-            $sum ++;
-        }
-        //判断十位为1的个数
-        if(floatval($i/10%10 == 1)){
-            $sum++;
-        }
+function GetUglyNumber_Solution($sun){
+     while ($sun = $sun%2){
+         $sun = $sun/2;
+         echo 2;
     }
-    print_r($sum);
-}
-return calFn(1,13);
+    while ($sun = $sun%3){
+         $sun = $sun/3;
+         echo 3;
+    }
+    while ($sun = $sun%5){
+         $sun = $sun/5;
+         echo 5;
+    }
+    if($sun = 1){
+         echo "是丑数";
+    }
+    else
+    {
+        echo "不是丑数";
+    }
 
+}
+return GetUglyNumber_Solution(5);
